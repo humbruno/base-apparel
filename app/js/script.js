96f07197e1056a6a15a2f80832223a09c8887d2e
Hum Bruno <56832348+humbruno@users.noreply.github.com>
2022-03-08 12:09:19 +0000

    Final code cleanups and added readme

--- app/js/script.js
+++ app/js/script.js
@@ -2,26 +2,22 @@ let email = document.querySelector("#email");
 const form = document.querySelector("form");
 const validation = document.querySelector(".validation-text");
 const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-const input = document.querySelector("input");
-const errorIcon = document.querySelector(".error-icon");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   if (email.value.match(pattern)) {
-    form.classList.add("success");
     form.classList.remove("invalid");
     validation.innerHTML = "Your interest has been registered!";
     validation.style.opacity = "1";
     validation.classList.add("true");
-    form.classList.remove("false");
+    validation.classList.remove("false");
   } else {
     form.classList.add("invalid");
-    form.classList.remove("success");
     validation.innerHTML = "Please provide a valid email.";
     validation.style.opacity = "1";
     validation.classList.remove("true");
-    form.classList.add("false");
+    validation.classList.add("false");
   }
 
   email.value = "";
